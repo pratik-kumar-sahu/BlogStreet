a5093cf9922f847a4ada503a6a3ad5f51259ca6a
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-03-13 22:44:17 +0530

    added some more middleware functions

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,10 +1,38 @@
 const Article = require('../models/articleModel');
 const Comment = require('../models/commentModel');
 const User = require('../models/userModel');
+const Portfolio = require('../models/portfolioModel');
 
-exports.articleOwner = (req, res) => {};
+exports.commentOwner = (req, res, next) => {};
 
-exports.commentOwner = (req, res) => {};
+exports.notAuthorized = (req, res, next) => {
+  User.findById(req.session.userId, (err, user) => {
+    if (err || !user) {
+      return res.redirect('/auth/login');
+    }
+    next();
+  });
+};
+
+exports.hasPortfolio = async (req, res, next) => {
+  const user = await User.findById(req.session.userId);
+  const foundPortfolio = await Portfolio.findOne({
+    portfolioAuthor: user.email,
+  });
+  if (foundPortfolio) {
+    return res.redirect('/portfolio/profile');
+  }
+  next();
+};
+
+exports.isLoggedIn = (req, res, next) => {
+  // console.log(req.session.userId);
+  if (req.session.userId) {
+    next();
+  } else {
+    res.redirect('/auth/login');
+  }
+};
 
 exports.checkUserSession = (req, res, next) => {
   User.findById(req.session.userId, (err, user) => {
